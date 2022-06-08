77bca4fd58695f9c27d8c9c3b464bd00f422a8bc
Manish Sharma <msmanisharma2601@outlook.com>
2022-06-08 11:24:13 +0530

    Part 3: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -69,6 +69,16 @@ public class Restaurant {
         return name;
     }
 
+    public int getTotalOfSelectedItems(List<String> itemList) {
+        int total = 0;
+        for (String itemName : itemList) {
+            Item item = findItemByName(itemName);
+            if(null != item)
+                total += item.getPrice();
+        }
+        return total;
+    }
+
     public Object calculateTotalOrderValue(List<String> cart) {
         return 0;
     }
